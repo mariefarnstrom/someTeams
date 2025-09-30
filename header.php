20fc382ceb97156a09daa6a42cb670187c7ce39d
Marie Färnström <fernstrm@hotmail.com>
2025-09-30 17:53:38 +0200

    First commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+
+$title = 'Some teams';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><? echo $title ?></title>
+</head>
+
+<body>
+    <header>
+        <h1><?= $title ?></h1>
+    </header>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__.'/header.php';
+?>
+
+
+
+
+
+
+<?php
+
+require __DIR__.'/footer.php';
