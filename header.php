7859fbef7b591cea111a7668a5aa8b3f5f98c88d
Marie Färnström <fernstrm@hotmail.com>
2025-10-05 15:16:50 +0200

    Added team logos, team names and style sheet

--- header.php
+++ header.php
@@ -9,6 +9,7 @@ $title = 'Some teams';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><? echo $title ?></title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
 
 require __DIR__.'/header.php';
+require __DIR__.'/data.php';
 ?>
 
-
+<section>
+    <?php foreach ($teams as $teamName => $team):
+        ?>
+        <div>
+            <img class="teamLogo" src="<?= $team['logo']; ?>" alt="<?= $teamName?> logotype" >
+            <br>
+            <a href=""> <?= $teamName;?></a>
+        </div>    
+        <?php endforeach
+    ?>
+</section>
 
 
 
